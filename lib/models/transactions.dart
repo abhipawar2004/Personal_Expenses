b787a7a6b0e97f77fda960d5ade80e86af00743b
Abhishek Pawar <pawarabhi2004@gmail.com>
2024-02-28 22:56:54 +0530

    Update transactions.dart

--- lib/models/transactions.dart
+++ lib/models/transactions.dart
@@ -5,11 +5,14 @@ class Transaction {
   final String title;
   final double amount;
   final DateTime date;
+  
 // constructor
+  
   Transaction({
   required this.id,
   required this.title,
   required this.amount,
   required this.date,
-   });
+   }
+ );
 }
